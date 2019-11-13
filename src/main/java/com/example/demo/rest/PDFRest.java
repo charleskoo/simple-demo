154ec0c4642239a93e53651f29a714cd46b40e07
charleskoo <charles.kou@yamibuy.com>
2019-11-13 15:26:39 +0800

    修改中英文字体文件

--- src/main/java/com/example/demo/rest/PDFRest.java
+++ src/main/java/com/example/demo/rest/PDFRest.java
@@ -1,26 +1,26 @@
 package com.example.demo.rest;
 
-import com.example.demo.service.PDFService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.PDFService;
+
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
 public class PDFRest {
 
-    @Autowired
-    private PDFService pDFService;
+	@Autowired
+	private PDFService pDFService;
 
-    @GetMapping("pdf")
-    public String pdfReader() {
-//        log.info("hello, pdf");
-        pDFService.read();
-        return "success";
-    }
+	@GetMapping("pdf")
+	public String pdfReader() {
+		String sourceFileName = "C:/Users/charles.kou/Desktop/pdf/3.2.P.8稳定性考察组合图谱.pdf";
+		String destFileName = "C:/Users/charles.kou/Desktop/pdf/result/result";
+		pDFService.read(sourceFileName, destFileName + 0 + ".pdf");
+		return "success";
+	}
 
 }
--- src/main/java/com/example/demo/service/PDFService.java
+++ src/main/java/com/example/demo/service/PDFService.java
@@ -2,14 +2,16 @@ package com.example.demo.service;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
-import org.apache.fontbox.ttf.TrueTypeCollection;
-import org.apache.fontbox.ttf.TrueTypeFont;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.FontFlagText;
+import com.google.common.collect.Lists;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,51 +19,82 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class PDFService {
 
-	private static final float INCH = 72;
+	private static final String FONT_FILE_PATH_SIMSUN = "C:/Users/charles.kou/Desktop/pdf/fonts/SimSun.ttf";
+	private static final String FONT_FILE_PATH_TIMES = "C:/Users/charles.kou/Desktop/pdf/fonts/TIMES.TTF";
 
-	public void read() {
+	public void read(String sourceFileName, String destFileName) {
 		PDDocument doc = null;
 		try {
-			File file = new File("C:\\Users\\charles.kou\\Desktop\\pdf\\origin.pdf");
+			File file = new File(sourceFileName);
 			doc = PDDocument.load(file);
-
-			log.info("PDF {} Loaded.", file.getName());
 
 			PDPage page = doc.getPage(0);
 
-//			float pw = page.getMediaBox().getUpperRightX();
-//			float ph = page.getMediaBox().getUpperRightY();
-
-			PDType0Font font;
-//			TrueTypeCollection trueTypeCollection = new TrueTypeCollection(new File("c:/windows/fonts/simsun.ttc"));
-//			TrueTypeFont trueTypeFont = trueTypeCollection.getFontByName("SimSun");
-//			if (null == trueTypeFont) {
-			String fontFilePath = "C:\\simsun.ttf";
-			font = PDType0Font.load(doc, new File(fontFilePath));
-//			} else {
-//				font = PDType0Font.load(doc, trueTypeFont, false);
-//			}
-//			trueTypeCollection.close();
-
+			PDType0Font simSunFont = PDType0Font.load(doc, new File(FONT_FILE_PATH_SIMSUN));
+			PDType0Font timesFont = PDType0Font.load(doc, new File(FONT_FILE_PATH_TIMES));
 			PDPageContentStream contents = new PDPageContentStream(doc, page, PDPageContentStream.AppendMode.PREPEND,
 					true);
 			contents.beginText();
-			contents.setFont(font, 10);
 			float offsetx = 40;
-			float offsety = 105;
+			float offsety = 50;
 			contents.newLineAtOffset(offsetx, offsety);
-			contents.showText("3.2.P.5.3.3-21   含量方法学验证/溶液稳定性/对照品溶液（0h） HPLC图谱");
+
+			String str = "附图3.2.P.8- 1稳定性试验/影响因素/0天/有关物质/空白溶剂HPLC图谱";
+			List<FontFlagText> textList = Lists.newArrayList();
+			for (int i = 0; i < str.length(); i++) {
+				char charAt = str.charAt(i);
+				FontFlagText text = new FontFlagText();
+				text.setText(String.valueOf(charAt));
+				if (isChinese(charAt)) {
+					text.setFont_flag(1);
+				}
+				textList.add(text);
+			}
+
+			for (FontFlagText fontFlagText : textList) {
+				if (fontFlagText.getFont_flag() == 1) {
+					contents.setFont(simSunFont, 10);
+				} else {
+					contents.setFont(timesFont, 10);
+				}
+				contents.showTextWithPositioning(new String[] { fontFlagText.getText() });
+			}
+
 			contents.endText();
 			contents.close();
 
-			doc.save("C:\\Users\\charles.kou\\Desktop\\pdf\\result\\result.pdf");
+			doc.save(destFileName);
 
 			doc.close();
+
 		} catch (IOException e) {
 
 			log.error(e.getMessage(), e);
 		}
 
+	}
+
+//	public static void main(String[] args) {
+//		String str = "附图3.2.P.8- 1稳定性试验/影响因素/0天/有关物质/空白溶剂HPLC图谱";
+//		List<FontFlagText> textList = Lists.newArrayList();
+//		for (int i = 0; i < str.length(); i++) {
+//			char charAt = str.charAt(i);
+//			FontFlagText text = new FontFlagText();
+//			text.setText(String.valueOf(charAt));
+//			if (isChinese(charAt)) {
+//				text.setFont_flag(1);
+//			}
+//			textList.add(text);
+//		}
+//
+//		List<FontFlagText> result = Lists.newArrayList();
+//		for (FontFlagText fontFlagText : textList) {
+//			System.out.println(fontFlagText.getFont_flag() + "\t" + fontFlagText.getText());
+//		}
+//	}
+
+	public static boolean isChinese(char c) {
+		return c >= 0x4E00 && c <= 0x9FA5;
 	}
 
 }
